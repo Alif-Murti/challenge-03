9766a1dc99a1c4f74e77ba6f52d47c0472673145
Alif Murti <alifmurti86@gmail.com>
2022-03-25 14:27:25 +0700

    detail added

--- server.js
+++ server.js
@@ -24,11 +24,6 @@ app.get("/car_form", function (req, res) {
   res.render("pages/car_form");
 });
 
-// about page
-app.get("/about", function (req, res) {
-  res.render("pages/about");
-});
-
 app.listen(8080);
 console.log("8080 is the magic port");
 
